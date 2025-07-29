37951ab6e64f7378c20a0d1bd7af99009a9441a5
bholevaibhav <vaibhav.bhole@anrxsolutions.com>
2025-07-29 19:50:55 +0530

    Relaxed ESLint rules for deployment

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,10 +2,10 @@
 module.exports = {
   extends: ['next/core-web-vitals'],
   rules: {
-    '@typescript-eslint/no-explicit-any': 'off',
-    'react/no-unescaped-entities': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
     '@typescript-eslint/no-unused-vars': 'warn',
     '@next/next/no-img-element': 'warn',
     'jsx-a11y/alt-text': 'warn',
+    'react/no-unescaped-entities': 'warn',
   },
 }
